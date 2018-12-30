9a00bf23532b3c08256deb88a5b2aca7985b209a
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-12-30 09:54:58 +0000

    # CHEVRE-82 ### Changed - ムビチケチャージ仕様にムビチケ属性を追加

--- src/factory/priceSpecification/movieTicketTypeChargeSpecification.ts
+++ src/factory/priceSpecification/movieTicketTypeChargeSpecification.ts
@@ -1,6 +1,23 @@
+import PaymentMethodType from '../paymentMethodType';
 import { IPriceSpecification as BaseSpecification } from '../priceSpecification';
 import PriceSpecificationType from '../priceSpecificationType';
 import VideoFormatType from '../videoFormatType';
+
+export interface IMovieTicket {
+    typeOf: PaymentMethodType.MovieTicket;
+    /**
+     * 購入管理番号(ムビチケ購入番号)
+     */
+    identifier?: string;
+    /**
+     * pinコード(ムビチケ暗証番号)
+     */
+    accessCode?: string;
+    /**
+     * 券種区分
+     */
+    serviceType?: string;
+}
 
 /**
  * ムビチケ券種区分チャージ仕様インターフェース
@@ -15,4 +32,8 @@ export type IPriceSpecification = BaseSpecification<PriceSpecificationType.Movie
      * 適用ムビチケ券種区分
      */
     appliesToMovieTicketType: string;
+    /**
+     * 適用ムビチケ
+     */
+    appliesToMovieTicket?: IMovieTicket;
 };
