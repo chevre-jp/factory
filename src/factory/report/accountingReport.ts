bbf3774cb1cc5e52f147bb287f8f18fc5cec658c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-10-06 06:39:00 +0000

    経理レポート検索条件拡張

--- src/factory/report/accountingReport.ts
+++ src/factory/report/accountingReport.ts
@@ -35,5 +35,6 @@ export interface ISearchConditions {
         paymentMethods?: {
             paymentMethodId?: { $eq?: string };
         };
+        seller?: { id?: { $eq?: string } };
     };
 }
