b83e105a8fa6755d997b93d8a7f552b1e507638f
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2022-05-13 13:31:07 +0900

    決済確定アクションを最適化

--- src/factory/action/interact/confirm/pay.ts
+++ src/factory/action/interact/confirm/pay.ts
@@ -1,10 +1,10 @@
 import * as ActionFactory from '../../../action';
 import { ActionType } from '../../../actionType';
 import { IPaymentMethod, ISimpleOrder } from '../../../order';
-import * as MovieTicketFactory from '../../../paymentMethod/paymentCard/movieTicket';
+// import * as MovieTicketFactory from '../../../paymentMethod/paymentCard/movieTicket';
 import { IInstrument } from '../../authorize/paymentMethod/any';
 
-export import IMovieTicket = MovieTicketFactory.IMovieTicket;
+// export import IMovieTicket = MovieTicketFactory.IMovieTicket;
 export type IAgent = ActionFactory.IParticipant;
 export type IRecipient = ActionFactory.IParticipant;
 export type IPurpose = ISimpleOrder;
@@ -17,25 +17,16 @@ export interface IPaymentService {
      * 決済方法
      */
     paymentMethod: IPaymentMethod;
-    /**
-     * ムビチケリスト
-     */
-    movieTickets?: IMovieTicket[];
+    // movieTickets?: IMovieTicket[];
 }
 export type IObject = IPaymentService[];
-// tslint:disable-next-line:no-empty-interface
-export interface IPotentialActions {
-}
-// tslint:disable-next-line:no-empty-interface
-export interface IResult {
-}
+export type IResult = any;
 export interface IAttributes extends ActionFactory.IAttributes<ActionType.ConfirmAction, IObject, IResult> {
     instrument?: IInstrument;
-    potentialActions?: IPotentialActions;
+    // potentialActions?: any;
     purpose: IPurpose;
 }
-
 /**
- * 決済確定アクションインターフェース
+ * 決済確定アクション
  */
 export type IAction = ActionFactory.IAction<IAttributes>;
