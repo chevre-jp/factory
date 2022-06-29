554b39dcd7d1d86c348509762739d4f6154d5bf5
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2022-06-29 16:16:59 +0900

    トークン検証アクションを拡張

--- src/factory/action/check/token.ts
+++ src/factory/action/check/token.ts
@@ -1,20 +1,26 @@
 import * as ActionFactory from '../../action';
 import * as CheckActionFactory from '../check';
 
-export type IAgent = ActionFactory.IParticipantAsWebApplication | ActionFactory.IParticipantAsPerson;
+export enum ObjectType {
+    Ticket = 'Ticket'
+}
+export type IAgent = ActionFactory.IParticipantAsWebApplication
+    | ActionFactory.IParticipantAsPerson
+    | ActionFactory.IParticipantAsProject;
 export interface IObject {
     token: string;
+    typeOf: ObjectType;
 }
 export type IResult = any;
 export type IError = any;
 /**
- * トークン確認アクション属性
+ * トークン検証アクション属性
  */
 export interface IAttributes extends CheckActionFactory.IAttributes<IObject, IResult> {
     agent: IAgent;
     object: IObject;
 }
 /**
- * トークン確認アクション
+ * トークン検証アクション
  */
 export type IAction = ActionFactory.IAction<IAttributes>;
