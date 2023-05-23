daf4ebb37a9b99c1a09a95be0523b1f9237554a6
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2023-05-23 17:27:47 +0900

    OfferItemConditionを追加

--- src/offerItemCondition.ts
+++ src/offerItemCondition.ts
@@ -6,16 +6,34 @@ import { ReservationType } from './reservationType';
 import { SortType } from './sortType';
 import { UnitCode } from './unitCode';
 
-type IGracePeriodBeforeStart = Pick<
+export type IGracePeriodBeforeStart = Pick<
     IQuantitativeValue<UnitCode.Sec>,
     'maxValue' | 'minValue' | 'typeOf' | 'unitCode'
 >;
+export interface IGracePeriodBeforeStartInDays {
+    period: Pick<
+        IQuantitativeValue<UnitCode.Day>,
+        'value' | 'typeOf' | 'unitCode'
+    >;
+    time: string; // HH:mm:ssZ
+    timezone: string; // Asia/Tokyo
+}
 export interface IItemOffered {
     typeOf: ProductType.EventService;
     serviceOutput?: {
         typeOf: ReservationType.EventReservation;
         reservationFor: {
+            /**
+             * n秒前設定
+             */
             gracePeriodBeforeStart?: IGracePeriodBeforeStart;
+            /**
+             * n日時前のHH:mm:ss設定
+             */
+            gracePeriodBeforeStartInDays?: {
+                max?: IGracePeriodBeforeStartInDays;
+                min?: IGracePeriodBeforeStartInDays;
+            };
         };
     };
 }
