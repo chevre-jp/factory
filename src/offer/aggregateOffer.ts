8900359be848099b841b4b6cc8704d43328605a9
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2023-09-08 07:21:49 +0900

    集計オファーを追加

--- src/offer/aggregateOffer.ts
+++ src/offer/aggregateOffer.ts
@@ -8,10 +8,14 @@ export type ISubOffer = IUnitPriceOffer;
  */
 export interface IAggregateOffer extends Pick<IOffer, 'typeOf' | 'project'> {
     typeOf: OfferType.AggregateOffer;
+    highPrice?: number;
+    lowPrice?: number;
+    offerCount?: number;
     /**
      * サブオファー
      * 基本的に1つの基本オファーが含まれる
      * 条件によるバリエーションが存在する場合、2つ以上のオファーが含まれる
      */
     offers: ISubOffer[]; // 追加(2023-09-01~)
+
 }
