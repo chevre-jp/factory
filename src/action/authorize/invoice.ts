f1366c3acc960cc1aaf2219af2ca435f60881f7d
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2024-06-13 09:34:38 +0900

    add authorizeInvoiceAction

--- src/action/authorize/invoice.ts
+++ src/action/authorize/invoice.ts
@@ -1,5 +1,6 @@
 import * as ActionFactory from '../../action';
 import { ActionType } from '../../actionType';
+import { AssetTransactionType } from '../../assetTransactionType';
 import { IInvoice } from '../../invoice';
 import { IEntryTranArgs, IEntryTranResult, IExecTranArgs, IExecTranResult, IRecipe as IAuthorizeInvoiceRecipe } from '../../recipe/authorizeInvoice';
 import { IRecipe as IAuthorizeInvoice3dsRecipe, ISecureTran2Args, ISecureTran2Result } from '../../recipe/authorizeInvoice3ds';
@@ -15,13 +16,17 @@ export type IAgent = ActionFactory.IParticipantAsProject;
 export type IObject = Pick<IInvoice, 'paymentMethodId' | 'typeOf'>;
 // tslint:disable-next-line:no-empty-interface
 export interface IResult { }
+export interface ISameAs extends ActionFactory.ISameAs {
+    typeOf: AssetTransactionType.Pay;
+}
 export interface IAttributes extends Pick<
     AuthorizeActionFactory.IAttributes<IObject, IResult>,
-    'agent' | 'error' | 'object' | 'result' | 'project' | 'typeOf'
+    'agent' | 'error' | 'object' | 'result' | 'project' | 'typeOf' | 'sameAs'
 > {
     typeOf: ActionType.AuthorizeAction;
     object: IObject;
     agent: IAgent;
+    sameAs: ISameAs;
 }
 /**
  * 請求承認アクション
