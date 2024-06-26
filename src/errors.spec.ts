44dc9de3d7d58cf75ad1bff2030316d984f2aa5a
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2024-06-26 17:25:00 +0900

    add errors.Internal

--- src/errors.spec.ts
+++ src/errors.spec.ts
@@ -11,6 +11,7 @@ import {
     Chevre,
     Forbidden,
     GatewayTimeout,
+    Internal,
     NotFound,
     NotImplemented,
     RateLimitExceeded,
@@ -241,6 +242,25 @@ describe('new UnauthorizedError()', () => {
     it('メッセージを指定しなくても、正しくインスタンス化できる', () => {
         const error = new Unauthorized();
         assert(error instanceof Unauthorized);
+        assert.equal(error.name, 'ChevreError');
+        assert.equal(typeof error.message, 'string');
+        assert.equal(typeof error.stack, 'string');
+    });
+});
+
+describe('new InternalError()', () => {
+    it('正しくインスタンス化できる', () => {
+        const message = 'test message';
+        const error = new Internal(message);
+        assert(error instanceof Internal);
+        assert.equal(error.message, message);
+        assert.equal(error.name, 'ChevreError');
+        assert.equal(typeof error.stack, 'string');
+    });
+
+    it('メッセージを指定しなくても、正しくインスタンス化できる', () => {
+        const error = new Internal();
+        assert(error instanceof Internal);
         assert.equal(error.name, 'ChevreError');
         assert.equal(typeof error.message, 'string');
         assert.equal(typeof error.stack, 'string');
